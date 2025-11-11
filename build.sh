0d94a3557821ddcbfaf3f35a4e5fb4efa700955a
Vivaan Malik <malikvivaan@gmail.com>
2025-11-11 17:35:37 +0530

    finallll

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-set -e  # this guy is a natural ctrl c :D
+set -e  # this guy is a natural ctrl c :D (exit if cmnd fails)
 
 SRC_DIR=src 
 INCLUDE_DIR=include
@@ -19,7 +19,7 @@ for src_file in $SRC_DIR/*.c; do
     if [ "$src_file" -nt "main" ]; then
         obj_file="$BUILD_DIR/$(basename ${src_file%.c}.o)"
         echo "    $((++count)) Compiling $src_file -> $obj_file"
-        $CC $CFLAGS -c "$src_file" -o "$obj_file" -mavx2 -mfma -lm -O3 -march=native -fopenmp
+        $CC $CFLAGS -c "$src_file" -o "$obj_file" -lm -O3 -march=native -fopenmp
         LIB_OBJS+=("$obj_file")
     fi
 done
@@ -29,10 +29,16 @@ ar rcs "$BUILD_DIR/lib$LIB_NAME.a" "${LIB_OBJS[@]}"
 
 echo "=== Compiling main program ==="
 MAIN_OBJ="$BUILD_DIR/main.o"
-$CC $CFLAGS -c "$MAIN_FILE" -o "$MAIN_OBJ" -mavx2 -mfma -lm -O3 -march=native -fopenmp
+$CC $CFLAGS -c "$MAIN_FILE" -o "$MAIN_OBJ" -lm -O3 -march=native -fopenmp
 
 echo "=== Linking executable ==="
-$CC "$MAIN_OBJ" -L$BUILD_DIR -l$LIB_NAME -mavx2 -mfma -o main -lm -O3 -march=native -fopenmp
-# $CC "$MAIN_OBJ" -L$BUILD_DIR -l$LIB_NAME -o main_debug -lm
+$CC "$MAIN_OBJ" -L$BUILD_DIR -l$LIB_NAME -o main -lm -fopenmp -march=native -fopenmp
+
+# mavx2 = lets compiler use 256 bit vector operations, faster math if auto vectorized (it generally is)
+# mfma = a*b + c in one instruction isntead of 2, faster
+# march=native = optimize as per ur cpu (the top 2 automatically)
+# O3 =  high optimization, can reorder loops and shi
+# lm = link math lib
+# fopenmp = enables multithreading
 
 echo "Build complete! Executable: main"
